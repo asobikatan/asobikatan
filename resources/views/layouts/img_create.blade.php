4b66371ce387c6bcf06fa6333ed898a46d0037e4
KazukiUSHIJIMA <2674je@gmail.com>
2018-09-09 10:50:55 +0630

    loading img-create final

--- resources/views/layouts/img_create.blade.php
+++ resources/views/layouts/img_create.blade.php
@@ -38,98 +38,119 @@
             }
         @endif
 
-        #loader-bg {
-          display: none;
-          position: fixed;
-          width: 100%;
-          height: 100%;
-          top: 0px;
-          left: 0px;
-          z-index: 1;
+        #loading{
+            display: none;
         }
-        #loader {
-          display: none;
-          position: fixed;
-          background-color: #fff;
-          width: 50%;
-          height: 50%;
-          top: 25%;
-          left: 25%;
-          text-align: center;
-          z-index: 2;
-          color: #000;
+
+        .loader,
+        .loader:before,
+        .loader:after {
+            border-radius: 50%;
+            width: 2.5em;
+            height: 2.5em;
+            -webkit-animation-fill-mode: both;
+            animation-fill-mode: both;
+            -webkit-animation: load7 1.8s infinite ease-in-out;
+            animation: load7 1.8s infinite ease-in-out;
         }
+        .loader {
+            color: #000;
+            font-size: 10px;
+            margin: 0 auto;
+            position: relative;
+            text-indent: -9999em;
+            -webkit-transform: translateZ(0);
+            -ms-transform: translateZ(0);
+            transform: translateZ(0);
+            -webkit-animation-delay: -0.16s;
+            animation-delay: -0.16s;
+        }
+        .loader:before,
+        .loader:after {
+            content: '';
+            position: absolute;
+            top: 0;
+        }
+        .loader:before {
+            left: -3.5em;
+            -webkit-animation-delay: -0.32s;
+            animation-delay: -0.32s;
+        }
+        .loader:after {
+            left: 3.5em;
+        }
+        @@-webkit-keyframes load7 {
+          0%,
+          80%,
+          100% {
+            box-shadow: 0 2.5em 0 -1.3em;
+          }
+          40% {
+            box-shadow: 0 2.5em 0 0;
+          }
+        }
+        @@keyframes load7 {
+          0%,
+          80%,
+          100% {
+            box-shadow: 0 2.5em 0 -1.3em;
+          }
+          40% {
+            box-shadow: 0 2.5em 0 0;
+          }
+        }
+
     </style>
+    <!-- 参考：https://projects.lukehaas.me/css-loaders/ -->
 </head>
 
 <body>
-    <div id="loader-bg">
-        <div id="loader">
-            読み込み中です……
-        </div>
-    </div>
-    <div id="wrap">
-        @if(isset($pic_errors))
-            <h2 class="error">画像の投稿に失敗しました</h2>
+    <div id="loading" class="loader">投稿しています……</div>
+    @if(isset($pic_errors))
+        <h2 class="error">画像の投稿に失敗しました</h2>
+    @endif
+    @if(isset($path))
+        <h2>画像の投稿に成功しました。</h2>
+        <p>任意の場所にタグを貼り付けて使用してください。<br>
+            もっと画像が必要ですか？同じ操作を繰り返すことで、複数枚の画像を投稿できます。</p>
+    @endif
+    <form action="/aso-repo/img-create" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <input type="hidden" name="user_id" value="{{$session_user->id}}">
+        <input type="hidden" name="aid" value="{{$aid}}">
+        <input type="file" name="pics" accept="image/png, image/jpeg, image/gif">
+        @if($ua == 'sp_objects.common')
+            <br>
         @endif
-        @if(isset($path))
-            <h2>画像の投稿に成功しました。</h2>
-            <p>任意の場所にタグを貼り付けて使用してください。<br>
-                もっと画像が必要ですか？同じ操作を繰り返すことで、複数枚の画像を投稿できます。</p>
-        @endif
-        <form action="/aso-repo/img-create" method="post" enctype="multipart/form-data">
-            {{csrf_field()}}
-            <input type="hidden" name="user_id" value="{{$session_user->id}}">
-            <input type="hidden" name="aid" value="{{$aid}}">
-            <input type="file" name="pics" accept="image/png, image/jpeg, image/gif">
-            @if($ua == 'sp_objects.common')
-                <br>
-            @endif
-            <input class="misc-btn" type="submit" alt="送信する" value="->画像を投稿->">
-            @isset($path)
-                <input id="copyTarget" type="text" value='<img src="{{$path}}" style="width: 30%; float: right;">'>
-                <button class="misc-btn" id="copyButton">タグをコピー</button>
-            @else
-                <input id="copyTarget" type="text" placeholder="ここにタグが出ます">
-            @endisset
-            ※10MByteまで
-        </form>
-        <script>
-            var button = document.getElementById('copyButton');
-            button.addEventListener('click', function(){
-                var copyTarget = document.getElementById('copyTarget');
-                var range = document.createRange();
-                range.selectNode(copyTarget);
-                window.getSelection().addRange(range);
-                document.execCommand('copy');
-                alert("コピーできました！ : " + copyTarget.value);
-            });
-        </script>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script>
-        $(function() {
-          var h = $(window).height();
-
-          $('#wrap').css('display','none');
-          $('#loader-bg ,#loader').height(h).css('display','block');
+        <input class="misc-btn" type="submit" alt="送信する" value="->画像を投稿->" onClick='disp_loading();'>
+        @isset($path)
+            <input id="copyTarget" type="text" value='<img src="{{$path}}" style="width: 30%; float: right;">'>
+            <button class="misc-btn" id="copyButton">タグをコピー</button>
+        @else
+            <input id="copyTarget" type="text" placeholder="ここにタグが出ます">
+        @endisset
+        ※10MByteまで
+    </form>
+    <script>
+        var button = document.getElementById('copyButton');
+        button.addEventListener('click', function(){
+            var copyTarget = document.getElementById('copyTarget');
+            var range = document.createRange();
+            range.selectNode(copyTarget);
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+            alert("コピーできました！ : " + copyTarget.value);
         });
+    </script>
+    <!-- 参考：https://dev.classmethod.jp/smartphone/iphone/safari-cut-copy/ -->
 
-        $(window).load(function () { //全ての読み込みが完了したら実行
-          $('#loader-bg').delay(800).fadeOut(800);
-          $('#loader').delay(600).fadeOut(300);
-          $('#wrap').css('display', 'block');
-        });
-
-        //10秒たったら強制的にロード画面を非表示
-        $(function(){
-          setTimeout('stopload()',10000);
-        });
-
-        function stopload(){
-          $('#wrap').css('display','block');
-          $('#loader-bg').delay(900).fadeOut(800);
-          $('#loader').delay(600).fadeOut(300);
+    <script>
+        function disp_loading()
+        {
+            document.getElementById("loading").style.display="block";
         }
-        </script>
-    </div>
+    </script>
+    <!-- 参考：https://www.pazru.net/js/DOM/7.html -->
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 </body>
